4432729aecf5bfcbf0377b5eb9e53fd35f9dc399
hotdogc1017 <hotdogc1017@gmail.com>
2025-01-03 10:09:17 +0800

    feat: update demo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Wheel from './components/Wheel.vue'
+
+const list = [
+  { name: 'gift 1', color: 'red' },
+  { name: 'gift 2', color: 'blue' },
+  { name: 'gift 3', color: 'green' },
+]
+
+const wheelRef = ref<InstanceType<typeof Wheel>>()
+
+async function handleClick() {
+  const { name } = (await wheelRef.value?.spin(1))!
+  alert(`You won ${name}`)
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
-  </div>
-  <Wheel :list="[{name: 'gift 1', color: 'red'}, {name: 'gift 2', color: 'blue'}, {name: 'gift 3', color: 'green'}]" />
+  <Wheel ref="wheelRef" :list="list" />
+  <button @click="handleClick">
+    Start
+  </button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+button {
+  margin-top: 1rem;
 }
 </style>
