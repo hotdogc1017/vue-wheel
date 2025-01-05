5881a3e185617ef2ee2381134fb50e68c23b8bed
hotdogc1017 <hotdogc1017@gmail.com>
2025-01-05 21:13:09 +0800

    feat: Example of an unfinished construction

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,80 @@ const list = [
   { name: 'gift 1', color: 'red' },
   { name: 'gift 2', color: 'blue' },
   { name: 'gift 3', color: 'green' },
+  { name: 'gift 4', color: 'orange' },
+  { name: 'gift 5', color: 'pink' },
 ]
 
 const wheelRef = ref<InstanceType<typeof Wheel>>()
+const timeout = ref<number>(2000)
+const settings = ref({
+  pointerColor: undefined,
+  randomOffsetAngle: undefined,
+})
 
-async function handleClick() {
-  const { name } = (await wheelRef.value?.spin(1))!
+async function handleClick(type: 'normal' | 'mock') {
+  const randomValue = Math.floor(Math.random() * list.length)
+
+  let name: string
+  if (type === 'normal') {
+    ({ name } = (await wheelRef.value?.spin(randomValue))!)
+  }
+  else if (type === 'mock') {
+    ({ name } = (await wheelRef.value?.spin(() => mockRequest(randomValue)))!)
+  }
   alert(`You won ${name}`)
+}
+
+async function mockRequest(value: number) {
+  return new Promise<number>((resolve) => {
+    setTimeout(() => {
+      resolve(value)
+    }, timeout.value)
+  })
 }
 </script>
 
 <template>
-  <Wheel ref="wheelRef" :list="list" />
-  <button @click="handleClick">
-    Start
-  </button>
+  <Wheel ref="wheelRef" :list="list" v-bind="settings" />
+  <p style="display: grid;">
+    <button @click="handleClick('normal')">
+      Start
+    </button>
+    <button @click="handleClick('mock')">
+      Async Start
+    </button>
+  </p>
+
+  <p>
+    <span style="font-size: 1.5rem;">Settings:</span>
+  </p>
+  <footer id="wheel_container_settings">
+    <label>
+      Timeout(ms):
+    </label>
+    <input v-model="timeout" type="number">
+    <label>
+      Pointer Color:
+    </label>
+    <input v-model="settings.pointerColor" type="color">
+    <label>
+      Random Offset Angle:
+    </label>
+    <input v-model="settings.randomOffsetAngle" type="number">
+  </footer>
 </template>
 
 <style scoped>
 button {
   margin-top: 1rem;
+}
+
+#wheel_container_settings {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+}
+#wheel_container_settings > label {
+  justify-self: end;
 }
 </style>
